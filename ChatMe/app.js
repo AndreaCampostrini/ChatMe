792fddc70b595df1e5440b5547c187f18e7cc954
Claudio Festi <123649462+ClaudioFesti1@users.noreply.github.com>
2023-01-26 15:40:11 +0000

    .

---
+++ ChatMe/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const config = require("./config.js");
+const socketio = require("socket.io");
+
+const app = express();
+
+app.use(express.static("public"));
+
+const server = app.listen(config.port, () => {
+  console.log("Server in ascolto sulla porta " + config.port);
+});
+
+const io = socketio(server);
+var nicknames = [];
+io.on("connection", (socket) => {
+  socket.on("register", (nome) => {
+    console.log("client connesso");
+    socket.nome = nome;
+    console.log("Client connesso:", socket.nome);
+    socket.emit(
+      "confirm",
+      "Registrazione avvenuta con successo. Benvenuto " + socket.nome
+    );
+    socket.broadcast.emit("newuser", socket.nome + " si è unito alla chat.");
+    io.emit("newuser", socket.nome);
+  });
+});
